e7b0f38f25a07752175d42af453df5240ff56866
Dmytro Pasiuta <dmitriy@Dmytros-MacBook-Pro.local>
2022-03-13 12:33:10 +0200

    managing checklists

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -10,34 +10,64 @@
     <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
         <li class="nav-item"><a class="nav-link" href="{{route('home')}}">
                 <svg class="nav-icon">
-                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-speedometer"></use>
-                </svg> Dashboard</a></li>
+                    <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer')}}"></use>
+                </svg>
+            {{__('Dashboard')}}</a></li>
+
         @if(auth()->user()->is_admin)
-        <li class="nav-title">Admin</li>
-        <li class="nav-group">
-            <a class="nav-link" href="{{route('admin.pages.index')}}">
+        <li class="nav-title">{{__('Admin')}}</li>
+        <li class="nav-group"><a class="nav-link" href="{{route('admin.pages.index')}}">
                 <svg class="nav-icon">
                     <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle')}}"></use>
-                </svg> Pages</a>
-
+                </svg>
+            {{__('Pages')}}</a>
         </li>
-            @endif
-        </svg> Base</a>
-        <ul class="nav-group-items">
-            <li class="nav-item"><a class="nav-link" href="base/breadcrumb.html"><span class="nav-icon"></span> Breadcrumb</a></li>
 
-        <li class="nav-item"><a class="nav-link" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-
+            <li class="nav-title">{{__('Manage Checklists')}}</li>
+            @foreach(\App\Models\ChecklistGroup::with('checklists')->get() as $group)
+                <li class="nav-group"><a class="nav-link nav-group-toggle" href="{{route('admin.checklist_groups.edit',$group->id)}}">
+                        <svg class="nav-icon">
+                            <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle')}}"></use>
+                        </svg> {{$group->name}}</a>
+                    <ul class="nav-group-items">
+                        @foreach($group->checklists as $checklist)
+                        <li class="nav-item"><a class="nav-link" href="{{route('admin.checklist_groups.checklists.edit', [$group,$checklist])}}">
+                                <span class="nav-icon"></span> {{$checklist->name}}
+                            </a>
+                        </li>
+                        @endforeach
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{route('admin.checklist_groups.checklists.create',$group)}}" > {{__('New checklist ')}}</a>
+                            </li>
+                    </ul>
+                </li>
+            @endforeach
+             <li class="nav-item">
+               <a class="nav-link" href="{{route('admin.checklist_groups.create')}}" > {{__('New checklist group')}}</a>
+             </li>
+        @endif
+        <li class="nav-group"><a class="nav-link nav-group-toggle" href="#">
                 <svg class="nav-icon">
-                    <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}"></use>
-                </svg> {{ __('Logout') }}</a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle')}}"></use>
+                </svg> Base</a>
+            <ul class="nav-group-items">
+
+                <li class="nav-item"><a class="nav-link" href="base/breadcrumb.html"><span class="nav-icon"></span> Breadcrumb</a></li>
+
+            </ul>
+        </li>
+        <li class="nav-item"><a class="nav-link"href="{{route('logout')}}"
+            onclick="event.preventDefault();
+             document.getElementById('logout-form').submit();">
+                <svg class="nav-icon">
+                    <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer')}}"></use>
+                </svg>
+            {{ __('Logout') }}
+            </a>
+            <form id="logout-form" action="{{route ('logout')}}" method="POST" class="d-none">
                 @csrf
             </form>
         </li>
 
     </ul>
-
 </div>
